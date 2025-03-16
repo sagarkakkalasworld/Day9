26ccdc8b0b6022a108ce0b84efc1427fc3c1b528
sagarkakkalasworld <sagarkakkalasworld@gmail.com>
2025-03-16 08:05:04 +0000

    Update deploy.sh

--- Bash/deploy.sh
+++ Bash/deploy.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
-cd /home/ubuntu/bashscripts/Day8/react-helmcharts/
+cd /home/ubuntu/bashscripts/Day9/react-helmcharts/
+aws s3 cp s3://gitcommittagbucket/old_value.txt .
+aws s3 cp s3://gitcommittagbucket/new_value.txt .
+old_value=$(cat old_value.txt)
+new_value=$(cat new_value.txt)
+sed -i "s/${old_value}/${new_value}/g" values.yaml
+aws s3 rm s3://gitcommitidsreactdemo/old_value.txt
+echo $new_value > old_value.txt
+aws s3 cp old_value.txt s3://gitcommittagbucket/
+rm old_value.txt new_value.txt
 helm upgrade react-helmcharts .
 
