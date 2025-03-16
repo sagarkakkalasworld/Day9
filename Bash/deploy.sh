60b35b4cc7a90f1114c3b787d0744ee481f0b1da
sagarkakkalasworld <sagarkakkalasworld@gmail.com>
2025-03-16 08:55:19 +0000

    Update deploy.sh

--- Bash/deploy.sh
+++ Bash/deploy.sh
@@ -5,7 +5,7 @@ aws s3 cp s3://gitcommittagbucket/new_value.txt .
 old_value=$(cat old_value.txt)
 new_value=$(cat new_value.txt)
 sed -i "s/${old_value}/${new_value}/g" values.yaml
-aws s3 rm s3://gitcommitidsreactdemo/old_value.txt
+aws s3 rm s3://gitcommittagbucket/old_value.txt
 echo $new_value > old_value.txt
 aws s3 cp old_value.txt s3://gitcommittagbucket/
 rm old_value.txt new_value.txt
