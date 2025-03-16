9d4b10969bc9997f62ed8b4c52c5c9b447533a98
sagarkakkalasworld <sagarkakkalasworld@gmail.com>
2025-03-16 08:01:54 +0000

    Update build.sh

--- Bash/build.sh
+++ Bash/build.sh
@@ -3,8 +3,15 @@ sudo docker rmi -f $(sudo docker images -q) ##this is not recommned step, i am d
 sudo rm -r gold ## these steps are not recommened instead you can modify script as shown below
 sudo mkdir gold
 cd gold/
-sudo git clone https://github.com/sagarkakkalasworld/Day7.git
-cd Day7/Code
+sudo git clone https://github.com/sagarkakkalasworld/Day9.git
+cd Day9/Code
+git_commit=$(sudo git rev-parse HEAD)
 sudo docker build -t react-microk8s -f golddockerfile .
-sudo docker tag react-microk8s:latest sagarkakkalasworld/react-microk8s:latest ##make sure you did docker login
-sudo docker push sagarkakkalasworld/react-microk8s:latest
+sudo docker tag react-microk8s:$git_commit sagarkakkalasworld/react-microk8s:$git_commit
+sudo docker push sagarkakkalasworld/react-microk8s:$git_commit
+aws s3 rm s3://gitcommittagbucket/new_value.txt
+sudo touch new_value.txt
+sudo chmod 777 new_value.txt
+sudo echo $git_commit > new_value.txt
+aws s3 cp new_value.txt s3://gitcommittagbucket/
+sudo rm new_value.txt
